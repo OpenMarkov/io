9365a4c5269708fd2790eca49db34dd15fba4707
Jorge Pérez <jorgepmartin@gmail.com>
2018-02-26 10:56:30 +0100

    Modifying and including new license of OpenMarkov

--- src/main/java/Dummy.java
+++ src/main/java/Dummy.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) CISIAD, UNED, Spain,  2018. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
+
 /**
  * Created by jorge on 02/12/2016 only for testing purposes.
  */
