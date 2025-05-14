45fcccb79908d1239ede7ee00b396d73f2f6ba06
Jose Andres <jamota@ccia.uned.es>
2025-05-14 11:57:16 +0200

    Re-adding the write of policies and changing the display of PotentialEditDialog (Impose policy)

--- src/main/java/org/openmarkov/io/probmodel/writer/PGMXWriter_0_2.java
+++ src/main/java/org/openmarkov/io/probmodel/writer/PGMXWriter_0_2.java
@@ -80,6 +80,7 @@ public class PGMXWriter_0_2 implements ProbNetWriter {
 		try {
 			writeXMLProbNet(probNet, root);
 			writeInferenceOptions(probNet, root);
+			writePolicies(probNet, root);
 			writeEvidence(probNet, evidences, root);
 			Document document = new Document(root);
 			XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
@@ -161,7 +162,28 @@ public class PGMXWriter_0_2 implements ProbNetWriter {
 	}
 
 
-
+	protected void writePolicies(ProbNet probNet, Element root) {
+		Element policiesElement = new Element(XMLTags.POLICIES.toString());
+		boolean existsAtLeastOnePolicy = false;
+		List<Variable> decisionVariables = probNet.getVariables(NodeType.DECISION);
+		for (Variable variable : decisionVariables) {
+			Node node = probNet.getNode(variable);
+			if (node.hasPolicy()) {
+				existsAtLeastOnePolicy = true;
+				List<Potential> decisionPotentials = node.getPotentials();
+				for (Potential decisionPotential : decisionPotentials) {
+					Element potentialElement = new Element(XMLTags.POTENTIAL.toString());
+					getPotential(probNet, decisionPotential, potentialElement);
+					potentialElement.setAttribute(XMLAttributes.ROLE.toString(),
+							decisionPotential.getPotentialRole().toString());
+					policiesElement.addContent(potentialElement);
+				}
+			}
+		}
+		if (existsAtLeastOnePolicy) {
+			root.addContent(policiesElement);
+		}
+	}
 	/**
 	 * Writes evidence nodes into XML format
 	 *
