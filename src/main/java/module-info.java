10d78eb10e30ac8c467da71dad85b935a3e53f00
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-12-08 03:20:37 +0100

    [Feature, Refactor] When starting the program, the Splash is almost immediately shown. In the past it was used to show after loading the bundles of StringDatabase, which was an expensive operation, leading users to think the program started more to open than it really did. For this matter, the bundles of StringDatabase are now lazily initialised. [Feature, Refactor] Replaced the old Preference system for LocalPreference, this makes so preferences are still compatible in O.S. that have no Backing Store (BS). For those withouth BS, preferences might be stored in the File System (FS) if allowed, and if not, they are saved as long as the session is active. Values are lazily initialized, allow any Class that is serializable. This is less verbose, reduces probable bugs and boosts maintanability. This drastically reduces code, for example, LastOpenFiles now work as nothing more than a List wrapper. [Refactor, Spelling] Replaced wrongly written names in Preferences for correct names. Names now use underscores (_) instead of spaces ( ) to avoid problems with certain File Systems. [Development tool] Added an executable function in IntegrationTest to see all of the preferences values, no longer requiring to do this in OpenMarkov's GUI. [Feature, Experimental] OpenMarkovs interface's position and dimension are saved between sessions. [Feature, Experimental] OpenMarkovs experimental dark mode is saved between sessions. [Refactor] Removed Message Window. [Feature, UX] Replaced the use of Windows (MDI) for Tabs (JTabbedPane). [Feature, UX] Added a close button on tabs. When closing a ProbNet tab that has changed, it prompts the user a chance to save it. [Feature, UX] Added a save button on ProbNet tabs. This button is only enabled when the network is modified (When the network is save it is not considered modified for this matter). [Feature, UX] Added a contextual menu on tabs with multiple options: Save, Save as..., Close, Close all tabs, Close all tabs but this, Close tabs to the left, and Close tabs to the right. All of the closing operations promts the user to save the network before closing every tab individually (if it is a network tab). Cancelling the save of a network whose tab was going to be closed causes the closing operation to be stopped. [Test, WIP] Added functionality for hot-reload of a development theme.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,21 @@
 open module org.openmarkov.io {
-	requires org.openmarkov.core;
-	
-	requires org.jdom2;
-	requires org.apache.commons.io;
-	requires java.desktop;
+    requires org.openmarkov.core;
+    
+    requires org.jdom2;
+    requires org.apache.commons.io;
+    requires java.desktop;
     requires jeval;
     requires org.jetbrains.annotations;
     requires org.openmarkov.annotation_processing;
     
     uses javax.swing.event.UndoableEditListener;
-	uses org.xml.sax.InputSource;
-	
-	exports org.openmarkov.io.probmodel.exception;
-	exports org.openmarkov.io.probmodel.reader;
-	exports org.openmarkov.io.probmodel.writer;
-	exports org.openmarkov.io.probmodel.strings;
-	
-	exports org.openmarkov.io.xmlbif.strings;
-	exports org.openmarkov.io.xmlbif;
+    uses org.xml.sax.InputSource;
+    
+    exports org.openmarkov.io.probmodel.exception;
+    exports org.openmarkov.io.probmodel.reader;
+    exports org.openmarkov.io.probmodel.writer;
+    exports org.openmarkov.io.probmodel.strings;
+    
+    exports org.openmarkov.io.xmlbif.strings;
+    exports org.openmarkov.io.xmlbif;
 }
